446f75d9a45c56190f8eacccfd8cdfcc04808980
turnerror <christianturner@hotmail.co.uk>
2020-05-29 11:55:27 +0100

    renamed Complete Button

--- src/Components/TodoCompleteButton/index.js
+++ src/Components/TodoCompleteButton/index.js
@@ -2,13 +2,13 @@ import React, {Component} from "react";
 
 export class TodoCompleteButton extends Component {
 
-    handleChecked = (e) => {
+    handleClick = () => {
         this.props.runOnClick(this.props.id);
     };
 
     render() {
         return (
-            <input onClick={this.handleChecked} type="button" value={this.props.btnValue}/>
+            <input onClick={this.handleClick} type="button" value={this.props.btnValue}/>
         )
     };
 }
